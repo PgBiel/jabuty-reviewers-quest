acc9e664bdcfb19e91c607927eee075ca5aeeb15
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-11-04 18:36:50 -0300

    sample flask app

---
+++ reviewers_quest/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    print("Path: {}".format(path))
+    return render_template("vue/index.html")
+
+def main():
+    app.run()
+
+if __name__ == '__main__':
+    main()
