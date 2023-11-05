15fae5e337c9a86b8f47b9cd0d039555e191e3ec
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-11-04 22:45:51 -0300

    scripts: format

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -2,21 +2,26 @@ import os
 import subprocess
 import sys
 
+
 def _execute(*args, inherit_args=True):
     if inherit_args:
         args = (*args, *sys.argv[1:])
     print(f"-- Script: running '{' '.join(args)}' --")
     os.execvp(args[0], args)
 
+
 backend_src_dir = "reviewers_quest"
+
 
 # Runs the app.
 def run():
     _execute("python", backend_src_dir + "/app.py")
 
+
 # Formats the backend code.
 def format():
     _execute("poetry", "run", "ruff", "format", backend_src_dir)
+
 
 # Checks the backend code.
 def check():
