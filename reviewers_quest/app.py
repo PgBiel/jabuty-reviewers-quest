56fa905b147633ceb8bab6fdf698069fa4b12dee
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-11-04 21:25:19 -0300

    run ruff format

--- reviewers_quest/app.py
+++ reviewers_quest/app.py
@@ -5,14 +5,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="../templates")
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def catch_all(path):
     print("Path: {}".format(path))
     return render_template("vue/index.html")
 
+
 def main():
     app.run()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
