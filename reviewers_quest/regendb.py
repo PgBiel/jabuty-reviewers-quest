14eabf67c1b6e0ee5b0c4b93402859af149abbcd
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-11-10 02:09:50 -0300

    backend,scripts: regendb

---
+++ reviewers_quest/regendb.py
@@ -0,0 +1,19 @@
+from .app import app
+from .db import db
+
+
+def regen_db() -> None:
+    """
+    Regenerates the database.
+    CAUTION: This will DELETE THE EXISTING DATA.
+    ONLY RUN THIS IN DEBUG. DO NOT RUN IN PRODUCTION.
+    """
+    with app.app_context():
+        print("[!] Dropping existing database...")
+        db.drop_all()
+        print("[!] Creating new database...")
+        db.create_all()
+
+
+if __name__ == "__main__":
+    regen_db()
--- scripts/__init__.py
+++ scripts/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 
-def _execute(*args, inherit_args=True):
+def _execute(*args, inherit_args=True) -> None:
     if inherit_args:
         args = (*args, *sys.argv[1:])
     print(f"-- Script: running '{' '.join(args)}' --")
@@ -13,15 +13,22 @@ backend_src_dir = "reviewers_quest"
 
 
 # Runs the app.
-def run():
+def run() -> None:
     _execute("python", backend_src_dir + "/app.py")
 
 
 # Formats the backend code.
-def format():
+def format() -> None:
     _execute("poetry", "run", "ruff", "format", backend_src_dir)
 
 
 # Checks the backend code.
-def check():
+def check() -> None:
     _execute("poetry", "run", "ruff", "check", backend_src_dir)
+
+# Regenerates the database.
+# CAUTION: leads to LOSS OF DATA.
+def regendb() -> None:
+    from reviewers_quest.regendb import regen_db
+    print("-- Script: regenerating database --")
+    regen_db()
