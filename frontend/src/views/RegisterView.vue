b0e5cea431c9685665a21533470e0fc02ac7f71c
LuizAugustoPapa <luiz.papa@aluno.puc-rio.br>
2023-11-20 13:08:07 -0300

    'Testando'

---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Register User</h1>
+    <RegisterUser />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+// Components
+import RegisterUser from "./RegisterUser.vue";
+
+export default defineComponent({
+  name: "RegisterView",
+  components: {
+    RegisterUser,
+  },
+});
+</script>
