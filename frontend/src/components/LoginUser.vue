783912259222d03df7fa78f5639fd3579ca611e1
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-11-23 01:02:40 -0300

    frontend: fix CI

--- frontend/src/components/LoginUser.vue
+++ frontend/src/components/LoginUser.vue
@@ -1,33 +1,31 @@
 <template>
-      <v-row justify="center">
-        <v-col cols="12" sm="8" md="6" lg="4">
-          <v-card>
-            <v-card-title class="headline text-center">
-              Login afre
-            </v-card-title>
-            <v-card-text>
-              <v-form ref="form" >
-                <v-text-field
-                  v-model="user.email"
-                  label="Email"
-                  type="email"
-                  :rules="rules.emailRules"
-                ></v-text-field>
-                <v-text-field
-                  v-model="user.password"
-                  label="Password"
-                  type="password"
-                ></v-text-field>
-                <v-btn color="indigo" type="submit" block> Sign Up </v-btn>
-                <div>
-                    <router-link to="/register">Cadastro</router-link>
-                </div>
-              </v-form>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-  </template>
+  <v-row justify="center">
+    <v-col cols="12" sm="8" md="6" lg="4">
+      <v-card>
+        <v-card-title class="headline text-center"> Login afre </v-card-title>
+        <v-card-text>
+          <v-form ref="form">
+            <v-text-field
+              v-model="user.email"
+              label="Email"
+              type="email"
+              :rules="rules.emailRules"
+            ></v-text-field>
+            <v-text-field
+              v-model="user.password"
+              label="Password"
+              type="password"
+            ></v-text-field>
+            <v-btn color="indigo" type="submit" block> Sign Up </v-btn>
+            <div>
+              <router-link to="/register">Cadastro</router-link>
+            </div>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -45,9 +43,7 @@ export default defineComponent({
           (v: string) => !!v || "Email is required",
           (v: string) => /.+@.+\..+/.test(v) || "Invalid e-mail.",
         ],
-        passwordRules: [
-          (v: string) => !!v || "Password is required",
-        ],
+        passwordRules: [(v: string) => !!v || "Password is required"],
       },
     };
   },
--- frontend/src/views/LoginPageView.vue
+++ frontend/src/views/LoginPageView.vue
@@ -1,17 +1,17 @@
 <template>
-    <LoginUser />
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from "vue";
-  
-  // Components
-  import LoginUser from "../components/LoginUser.vue";
-  
-  export default defineComponent({
-    name: "LoginView",
-    components: {
-      LoginUser,
-    },
-  });
-  </script>
+  <LoginUser />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+// Components
+import LoginUser from "../components/LoginUser.vue";
+
+export default defineComponent({
+  name: "LoginView",
+  components: {
+    LoginUser,
+  },
+});
+</script>
