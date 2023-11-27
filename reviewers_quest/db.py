65101139226dca19866f158b2b5e8a0d796eebdd
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-11-27 00:34:07 -0300

    backend/db: make fields non-nullable

--- reviewers_quest/db.py
+++ reviewers_quest/db.py
@@ -43,15 +43,15 @@ class Game(BaseModel):
     """Represents a game in the database."""
 
     game_id = sql.Column(sql.Integer, primary_key=True)
-    name = sql.Column(sql.String(256))
-    release_year = sql.Column(sql.Integer)
-    developer = sql.Column(sql.String(256), nullable=True)
-    publisher = sql.Column(sql.String(256), nullable=True)
-    genre = sql.Column(sql.String(256))
+    name = sql.Column(sql.String(256), nullable=False)
+    release_year = sql.Column(sql.Integer, nullable=False)
+    developer = sql.Column(sql.String(256), nullable=False)
+    publisher = sql.Column(sql.String(256), nullable=False)
+    genre = sql.Column(sql.String(256), nullable=False)
     reviews: orm.Mapped[list["Review"]] = orm.relationship()
 
     # note: the value of 'func.now()' will be determined by SQLite on insertion.
-    created_at = sql.Column(sql.DateTime(timezone=True), server_default=func.now())
+    created_at = sql.Column(sql.DateTime(timezone=True), server_default=func.now(), nullable=False)
 
     def __repr__(self) -> str:
         return f"Game(id={self.game_id}, name={self.name})"
@@ -61,10 +61,10 @@ class User(UserMixin, BaseModel):
     """Represents a user which can log into the website."""
 
     user_id = sql.Column(sql.Integer, primary_key=True)
-    email = sql.Column(sql.String(100), unique=True)
-    password = sql.Column(sql.String(100))
-    name = sql.Column(sql.String(1000))
-    created_at = sql.Column(sql.DateTime(timezone=True), server_default=func.now())
+    email = sql.Column(sql.String(100), unique=True, nullable=False)
+    password = sql.Column(sql.String(100), nullable=False)
+    name = sql.Column(sql.String(1000), nullable=False)
+    created_at = sql.Column(sql.DateTime(timezone=True), server_default=func.now(), nullable=False)
     reviews: orm.Mapped[list["Review"]] = orm.relationship(back_populates="user")
 
     def __repr__(self) -> str:
@@ -75,10 +75,10 @@ class Review(BaseModel):
     """Represents a user's review on a specific game."""
 
     review_id = sql.Column(sql.Integer, primary_key=True)
-    game_id: orm.Mapped[int] = orm.mapped_column(sql.ForeignKey(Game.game_id))
-    author_id: orm.Mapped[int] = orm.mapped_column(sql.ForeignKey(User.user_id))
-    stars = sql.Column(sql.Integer, sql.CheckConstraint("stars >= 0 AND stars <= 5"))
-    body = sql.Column(sql.String(100))
+    game_id: orm.Mapped[int] = orm.mapped_column(sql.ForeignKey(Game.game_id), nullable=False)
+    author_id: orm.Mapped[int] = orm.mapped_column(sql.ForeignKey(User.user_id), nullable=False)
+    stars = sql.Column(sql.Integer, sql.CheckConstraint("stars >= 0 AND stars <= 5"), nullable=False)
+    body = sql.Column(sql.String(100), nullable=False)
     user: orm.Mapped[User] = orm.relationship(back_populates="reviews")
 
     def __repr__(self) -> str:
