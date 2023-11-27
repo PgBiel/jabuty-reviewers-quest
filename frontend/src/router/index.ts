3f41efebe5f2bf81b628e8d0928c4e13ffc89567
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-11-27 01:51:39 -0300

    frontend: point home to GameListView

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 import RegisterUserView from "../views/RegisterUserView.vue";
 import GameListView from "../views/GameListView.vue";
 
@@ -7,7 +6,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: GameListView,
   },
   {
     path: "/about",
