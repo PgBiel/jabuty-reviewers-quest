933766c823aa173f0d20a7628fdb4c164fdd1e4a
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-11-27 00:43:51 -0300

    backend/api: add reviews endpoint

--- reviewers_quest/api/game.py
+++ reviewers_quest/api/game.py
@@ -21,3 +21,18 @@ def get_game(game_id: int) -> dict:
         Game.query.get_or_404(game_id, "Jogo não encontrado"),
         keys=("game_id", "name", "release_year", "publisher", "developer", "genre"),
     )
+
+
+@app.route("/api/game/<int:game_id>/reviews")
+def get_reviews(game_id: int) -> list[dict]:
+    """
+    Returns a list of reviews for this game.
+
+    :param game_id: ID of the game to obtain reviews for.
+    :return: List of reviews for that game.
+    """
+    game: Game = Game.query.get_or_404(game_id, "Jogo não encontrado")
+    return [
+        model_to_dict(review, keys=("review_id", "game_id", "author_id", "stars", "body"))
+        for review in game.reviews
+    ]
