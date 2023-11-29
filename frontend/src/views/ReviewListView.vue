720d48728dd16025aefef1133cb088e3eac55778
Jesmius <lucas.falvaren@gmail.com>
2023-11-28 22:58:52 -0300

    ReviewListView feita

---
+++ frontend/src/views/ReviewListView.vue
@@ -0,0 +1,19 @@
+<template>
+  <NavBar />
+  <ReviewList />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import ReviewList from "../components/ReviewList.vue";
+import NavBar from "../components/NavBar.vue";
+
+export default defineComponent({
+  name: "ReviewListView",
+  components: {
+    ReviewList,
+    NavBar,
+  },
+});
+</script>
