f381b7ce41ae11ca5326198597319f84f0aa2be7
Jesmius <lucas.falvaren@gmail.com>
2023-11-28 22:49:48 -0300

    Mock do ReviewList retirado

--- frontend/src/components/ReviewList.vue
+++ frontend/src/components/ReviewList.vue
@@ -3,46 +3,46 @@
     <v-col> </v-col>
     <v-card
       v-for="review in reviews"
-      :key="review.title"
+      :key="review.review_id"
       class="mx-auto"
       max-width="500"
     >
       <v-img class="align-start text-black" height="200">
         <v-row align="end" class="spacer" no-gutters>
-          <v-avatar color="purple">
-            <span class="text-h5">{{ review.iniciais }}</span>
+          <v-avatar color="grey">
+            <v-icon icon="mdi-account-circle"></v-icon>
           </v-avatar>
-          <v-card-title>{{ review.title }}</v-card-title>
         </v-row>
-        <v-rating v-model="review.rating"></v-rating>
-        <v-card-text>{{ review.text }} </v-card-text>
+        <v-rating v-model="review.stars"></v-rating>
+        <v-card-text>{{ review.body }} </v-card-text>
       </v-img>
     </v-card>
   </v-container>
 </template>
 
 <script lang="ts">
+import { Review } from "../common/types";
 import { defineComponent } from "vue";
 export default defineComponent({
-  name: "ReviewCard",
   data() {
-    // Valores mockados por enquanto
     return {
-      reviews: [
-        {
-          iniciais: "L",
-          title: "Titulo",
-          rating: 4,
-          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
-        },
-        {
-          iniciais: "T",
-          title: "Titulo 2",
-          rating: 4,
-          text: " consectetur adipiscing elit, sed do eiusmod",
-        },
-      ],
+      reviews: [] as Review[],
     };
+  },
+  methods: {
+    async getReviews() {
+      // Essa parte acho q está errada, tenho que extrair o game_id da URL
+      const gameResponse = await fetch("/api/game");
+      const game = await gameResponse.json();
+      const reviewsResponse = await fetch(
+        "/api/game/" + game.game_id + "/reviews",
+      );
+      const reviews: Review[] = await reviewsResponse.json();
+      this.reviews = reviews;
+    },
+    created() {
+      this.getReviews();
+    },
   },
 });
 </script>
