a5d11acbfa25edc89a71db8860f812bcd9e551a6
PgBiel <9021226+PgBiel@users.noreply.github.com>
2023-12-06 04:03:47 -0300

    frontend/UserProfile: ajustes de texto

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -7,13 +7,17 @@
         </v-avatar>
       </v-col>
       <v-row md="4" class="mx-4" no-gutters>
-        <v-list-item class="text-black">{{ user.name }}</v-list-item>
+        <v-list-item class="text-weight-bold">{{ user.name }}</v-list-item>
         <v-col></v-col>
         <v-btn @click="logout" rounded="rounded-lg" color="red"> Logout </v-btn>
       </v-row>
-      <v-row md="4" class="pl-6 my-3" no-gutters> {{ user.bio }} </v-row>
+      <v-row md="4" class="pl-6 my-3" no-gutters>
+        {{ user.bio || "Nenhuma bio" }}
+      </v-row>
       <v-row md="4" class="pl-6 my-4 ga-2" no-gutters>
-        <v-chip color="primary"> {{ user.interests }} </v-chip>
+        <v-chip color="primary">
+          {{ user.interests || "Nenhum interesse" }}
+        </v-chip>
       </v-row>
     </v-card>
   </v-container>
